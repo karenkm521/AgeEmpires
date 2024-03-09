b3b757155474d0bf92897162793a6cb78c8bf109
Karen Martinez <111015703+karenkm521@users.noreply.github.com>
2024-03-08 20:30:58 -0600

    Add files via upload

---
+++ age.js
@@ -0,0 +1,3 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.navList').classList.toggle('active'); // Activa/desactiva la clase 'active' en la lista de navegación
+});
